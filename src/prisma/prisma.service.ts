6503c9562923d9610ab5d2ab13a249d8f4247d24
Your Name <ahmad.diaa1972@gmail.com>
2026-03-14 23:14:02 +0200

    Refactor code structure for improved readability and maintainability

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,9 +1,29 @@
-import { Injectable } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
-
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { PrismaClient } from '@/generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import 'dotenv/config';
 @Injectable()
-export class PrismaService extends PrismaClient {
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    const connectionString = process.env.DATABASE_URL;
+
+    if (!connectionString) {
+      throw new Error('DATABASE_URL is not set');
+    }
+
+    const adapter = new PrismaPg({ connectionString });
+
+    super({ adapter });
+  }
+
   async onModuleInit() {
     await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
   }
 }
